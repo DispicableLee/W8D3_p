957a7bff15ba5b43ef98794025c037757db9000a
DispicableLee <lee.robert053@gmail.com>
2023-10-11 17:27:29 -0400

    done

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdBubbleSort.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdBubbleSort.js
@@ -27,25 +27,29 @@ function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
   // Do an "async loop":
   // 1. If (i == arr.length - 1), call outerBubbleSortLoop, letting it
   //    know whether any swap was made.
+  let swapped = madeAnySwaps
   const isGreaterThan = (answer) => {
     if(answer===true){
       temp = arr[i+1]
       arr[i+1] = arr[i]
       arr[i] = temp
+      swapped = true
     }
   }
   
   if(i === arr.length - 1){
-    outerBubbleSortLoop(madeAnySwaps)
+    outerBubbleSortLoop(swapped)
   }else{
     askIfGreaterThan(arr[i], arr[i+1], (isGreaterThan))
-    innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop)
+    innerBubbleSortLoop(arr, i+1, swapped, outerBubbleSortLoop)
   }
-// ⁡⁢⁣⁣  // 2. Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
-//   //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
-//   //    continue the inner loop. You'll want to increment i for the
-//   //    next call, and possibly switch madeAnySwaps if you did swap.⁡⁡
+  // 2. Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
+  //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
+  //    continue the inner loop. You'll want to increment i for the
+  //    next call, and possibly switch madeAnySwaps if you did swap.⁡⁡
 }
+
+
 
 // Once you're done testing innerBubbleSortLoop, write outerBubbleSortLoop.
 // Once you're done testing outerBubbleSortLoop, write absurdBubbleSort.
