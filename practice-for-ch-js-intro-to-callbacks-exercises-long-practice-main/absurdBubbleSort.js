59cb3ccab936827ef7ccab9eaaa05a06bfb6e4f4
DispicableLee <lee.robert053@gmail.com>
2023-10-11 15:28:49 -0400

    my eyes are rolling into the back of my skull

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdBubbleSort.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdBubbleSort.js
@@ -9,17 +9,42 @@ const reader = readline.createInterface({
 function askIfGreaterThan(el1, el2, callback) {
   // Prompt user to tell you whether el1 > el2; pass true back to the
   // callback if true; else false.
+  reader.question(`is ${el1} greater then ${el2} y/n?`,(answer)=>{
+    if(answer==='y'){
+      callback(true)
+      reader.close()
+    }else{
+      callback(false)
+      reader.close()
+    }
+  })
 }
+
+askIfGreaterThan(1, 2, (e)=>console.log(e))
 
 // Once you're done testing askIfGreaterThan with dummy arguments, write this.
 function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
   // Do an "async loop":
   // 1. If (i == arr.length - 1), call outerBubbleSortLoop, letting it
   //    know whether any swap was made.
-  // 2. Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
-  //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
-  //    continue the inner loop. You'll want to increment i for the
-  //    next call, and possibly switch madeAnySwaps if you did swap.
+  const isGreaterThan = (answer) => {
+    if(answer===true){
+      temp = arr[i+1]
+      arr[i+1] = arr[i]
+      arr[i] = temp
+    }
+  }
+  
+  if(i === arr.length - 1){
+    outerBubbleSortLoop(madeAnySwaps)
+  }else{
+    askIfGreaterThan(arr[i], arr[i+1], (isGreaterThan))
+    innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop)
+  }
+// ⁡⁢⁣⁣  // 2. Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
+//   //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
+//   //    continue the inner loop. You'll want to increment i for the
+//   //    next call, and possibly switch madeAnySwaps if you did swap.⁡⁡
 }
 
 // Once you're done testing innerBubbleSortLoop, write outerBubbleSortLoop.
